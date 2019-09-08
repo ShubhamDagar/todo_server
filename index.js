a0362ab418f369f1dcf03abecd678f4cf0a8470f
ShubhamDagar <shubhamdagarnsit@gmail.com>
2019-09-08 14:05:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded());
 app.use(express.static('assets'));
 
 //Handling all get request of the port
-app.get('/', function(req, res) {
-    element.find({}, function(err, allElements) {
+app.get('/', function (req, res) {
+    element.find({}, function (err, allElements) {
         if (err) {
             console.log("Error in fetching data from Database");
             return;
@@ -25,23 +25,40 @@ app.get('/', function(req, res) {
 });
 
 //Adding element to the Database
-app.post('/create-task', function(req, res){
+app.post('/create-task', function (req, res) {
     console.log('Entered values', req.body);
-    element.create(req.body, function(err, newElement){
-        if(err)
-        {
+    element.create(req.body, function (err, newElement) {
+        if (err) {
             console.log('Error in creating new task', err);
             return;
         }
-        else
-        {
-            console.log('*******', newElement);
+        else {
             return res.redirect('back');
         }
     })
-})
+});
+//Deleting single task
+app.get('/delete-task/', function (req, res) {
+    let id = req.query.id;
+    element.findByIdAndDelete(id, function (err) {
+        if (err) {
+            console.log('Error in deleting task from DB', err);
+            return;
+        }
+        return res.redirect('back');
+    });
+});
+app.get('/delete-all', function(req, res){
+    element.deleteMany({}, function(err){
+        if (err) {
+            console.log('Error in deleting task from DB');
+            return res.redirect('back');
+        }
+        return res.redirect('back');
+    });
+});
 //Starting server at port 8000
-app.listen(port, function(err) {
+app.listen(port, function (err) {
     if (err)
         console.log(`Error in starting server at port: ${port}`);
     else
